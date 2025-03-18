51f46ef810c84d34c221390ecc765c2c0efe0f36
raykaris <ryanmuthoni050@gmail.com>
2025-03-18 20:06:33 +0300

    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Sneakers','Official Men & Women Wear','Children wear','Ladies Fashion wears'],
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 1000,
+    loop: true
+});
